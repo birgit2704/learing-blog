ab5facba62eef8160c3235677e9da6da762f0187
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-07-07 09:54:59 +0200

    make read-more btn work across all pages

--- about.js
+++ about.js
@@ -1,3 +1,13 @@
 import { renderBlogsOneToThree } from "./sharedFunctions.module.js";
 
+let selectedBlog = "";
+
 renderBlogsOneToThree();
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.btn) {
+    selectedBlog = e.target.dataset.btn;
+    localStorage.setItem("blogId", selectedBlog);
+    window.location = "./article.html";
+  }
+});
