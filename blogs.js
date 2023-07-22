300b7dd7480318e6c3ce9e9f9af3ae51d595ebeb
Birgit <birgit.bartenschlager@web.de>
2023-07-22 11:43:46 +0200

    update blogs.js

--- blogs.js
+++ blogs.js
@@ -15,6 +15,7 @@ export const blogs = [
     <p>I love coding, so it doesn't take much to stay committed. But of course I have those moments when everything seems too much, when I am frustrated or burnt out. Then I go back to the many projects that I have built up to now. Mostly, I think: Wow, I have built this? In just 12 months I have learned to create things like that? Magnificent! I also look at peers' projects and think: I could code that, too! Most of the time, this is enough to get me going again.</p> 
           `,
     id: 0,
+    orderNo: 0,
   },
   {
     img: "./images/article-image-03.png",
@@ -28,6 +29,7 @@ export const blogs = [
     <p>I am waiting for the day when I sign an employment contract, then I'll invite both of them to something really fancy with the money I GET and I'll clarify that I will NEVER be able to hack the Pentagon.</p>
     `,
     id: 1,
+    orderNo: -1,
   },
   {
     img: "./images/article-image-02.png",
@@ -40,6 +42,7 @@ export const blogs = [
     <p>I said, no thanks and that I was studying because I wanted to become a software developer. After I had hung up, I was very upset. Why did I say I WANTED to become a software developer? I was finishing my 9-module course and the only thing left for me to do was to finish the capstone project to get the certificate. What I should have said: "no thanks to your job offer, I AM a software developer and we should talk about your web page, I have seen it can be improved. I have some ideas. When can we meet?" Why am I not confident like this?</p>
     <p>So I decided to get more confidence. I eat an apple every day (not to keep the doctor away, ha-ha) and with every bite I say to myself: "I am a software developer" Do you know what? This simple and - admittedly stupid - routine has changed the way I think of myself. I AM a software developer now. - Just not yet with a job.</p>`,
     id: 2,
+    orderNo: -2,
   },
   {
     img: "./images/article-image-01.png",
@@ -59,17 +62,22 @@ export const blogs = [
     <p>And Sunday's are off. It is difficult to keep away from the keyboard for a whole day, but I do have a life, after all.🏖️</p>
     `,
     id: 3,
+    orderNo: -3,
   },
-
   {
     img: "https://images.unsplash.com/photo-1485988412941-77a35537dae4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGNvbXB1dGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
     alt: "laptop on a wooden table",
-    date: "coming soon",
-    header: "Dealing with lacking recognition",
-    shortText:
-      "When family and friends are not so excited about your projects as you are.",
-    longText: `<p>coming soon</p>`,
+    date: "July 22, 2023",
+    header: "The value of feedback",
+    shortText: `The difference between feedback and <i>feedback</i>`,
+    longText: `<p>I was doing a course where fellow students had to evaluate each other's projects. One project was a site for a jewellery shop. I outdid myself and incorporated a lot of really amazing hovering and other effects, because I loved what did and - why not admit it - to show off what I could accomplish. I was sure, the others would be flashed.💎</p>
+    <p>Two of feedbacks I received were "nice job" - not exactly the praise I had hoped for, but still... The third evaluator wrote that my site did not have a modern look. NOT MODERN!!! I felt like punching him in his nose.👊 Judging from his username this guy lived on the other side of the globe. His luck!</p>
+    <p>A few days later I had another look at my project and seeing it with fresh eyes, I had to admit that he indeed was right. The playfulness of the effects did not convey the sharp, matter-of-fact and straight-forward interface that modern sites have. In all of my follow-up projects I have had this in the back of my mind and I think all projects have turned out better for this.❤️</p>
+    <p>One day at work, I was commenting to my collegues about a problem I had with my husband. They were all on my side and said how right I was. Of course they were, because they liked me. All but one. One collegue caught me alone at the coffee machine and told me to analyze the problem from my husband's point of view. First, I was surprised. Later, on my way home, I did what he had suggested, and realized there was another side to the coin🪙. The next day, I explicitly thanked that collegue for his honest opinion.</p> 
+    <p>When my son was a pre-scholar he hated to make paintings and only did it when 'forced' in kindergarden, quickly and dispassionately. Seeing those pictures, I always said how good they were. One day he said to me: "Mom, you praise ALL my pictures. How will I know when I do have a really good one?" So why did I praise his pictures? Not because they were good, but because I love him and I wanted to encourage him.🖌️</p>
+    <p>My conclusions of those three unrelated experiences are: most 'good' feedback is a sign of sympathy or lack of interest or time to engage. 'Bad' feedback is valuable, you have to have the courage to really consider and evaluate it. - And there is this feedback, when the others just say 'WOW'. That happened to me when I learned to draw and - thanks to an excellent teacher - turned from dawing stick figures to awesome portraits in no time. I really hope that you and me will have this kind of feedback soon on our coding projects💻. In the meantime, I am thankful for all honest feedback.</p>`,
     id: 4,
+    orderNo: -4,
   },
   {
     img: "https://images.unsplash.com/photo-1649940652572-7603b62c0a85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjE2fHxjb21wdXRlciUyMGJsYXV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
@@ -80,6 +88,7 @@ export const blogs = [
       "Ready to apply for the job? Not ready to apply? Never ready? Ever ready? Ready!",
     longText: `<p>coming soon</p>`,
     id: 5,
+    orderNo: -5,
   },
   {
     img: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
@@ -90,6 +99,7 @@ export const blogs = [
       "So here it is: I'll reveal my age and how it influences my study and job-searching process.",
     longText: `<p>coming soon</p>`,
     id: 6,
+    orderNo: -1,
   },
   {
     img: "https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvbXB1dGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
@@ -99,5 +109,6 @@ export const blogs = [
     shortText: "just here to prove that the 'view more' button works 😉",
     longText: `<p>coming later</p>`,
     id: 7,
+    orderNo: -7,
   },
 ];
